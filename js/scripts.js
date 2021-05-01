7fcd943d326d75a72764ecb645f652390602ea3c
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-05-01 13:00:19 -0700

    clear old codes and add pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-alert("hello world")
-let favoriteFood="sushi"
-document.write(favoriteFood)
+let pokemonList=[
+    {name='balbasure',
+      height=8,
+      type=['grass','poision']},
+
+    {name='charizard',
+      height=20,
+      type=['fire','dragon']},
+
+    {name='butterfree',
+      height=15,
+      type=['bug','flying']}
+]
