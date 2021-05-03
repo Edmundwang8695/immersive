88724babfde7dfc775e2ae0bd2c7f959ac908574
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-05-02 23:07:52 -0700

    added array for loop so html will show pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 let pokemonList=[
-    {name='balbasure',
-      height=8,
-      type=['grass','poision']},
+  {name:'balbasure',
+    height:7,
+    type:['grass','poision']},
 
-    {name='charizard',
-      height=20,
-      type=['fire','dragon']},
+  {name:'charizard',
+    height:20,
+    type:['fire','dragon']},
 
-    {name='butterfree',
-      height=15,
-      type=['bug','flying']}
+  {name:'butterfree',
+    height:15,
+    type:['bug','flying']}
 ]
+for (let i=0 ; i<pokemonList.length;i++){
+  if(pokemonList[i].height>16){
+      document.write( '<p>' + pokemonList[i].name + ' Height: ' + pokemonList[i].height + ' -Wow, this is big </p>')
+  }
+  else if(pokemonList[i].height<8){
+      document.write('<p>'+ pokemonList[i].name + ' Height: ' + pokemonList[i].height + ' -This is a small pokemon</p>')
+  }else{document.write('<p>' + pokemonList[i].name + ' Height: '+  pokemonList[i].height + ' -This is a average size pokemon</p>')}
+}
