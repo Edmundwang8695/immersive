799bb4ba40aa9b8b5f77a7ac5c422ac9d5bab2d2
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-05-09 22:58:10 -0700

    adding forEach loop and return function to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
-let pokemonList=[
-  {name:'balbasure',
-    height:7,
-    type:['grass','poision']},
-
-  {name:'charizard',
-    height:20,
-    type:['fire','dragon']},
-
-  {name:'butterfree',
-    height:15,
-    type:['bug','flying']}
-]
-for (let i=0 ; i<pokemonList.length;i++){
-  if(pokemonList[i].height>16){
-      document.write( '<p>' + pokemonList[i].name + ' Height: ' + pokemonList[i].height + ' -Wow, this is big </p>')
-  }
-  else if(pokemonList[i].height<8){
-      document.write('<p>'+ pokemonList[i].name + ' Height: ' + pokemonList[i].height + ' -This is a small pokemon</p>')
-  }else{document.write('<p>' + pokemonList[i].name + ' Height: '+  pokemonList[i].height + ' -This is a average size pokemon</p>')}
-}
+let pokemonRespository = (function(){ 
+  let pokemonList=[
+    {name:'balbasure',
+      height:7,
+      type:['grass','poision']},
+  
+    {name:'charizard',
+      height:20,
+      type:['fire','dragon']},
+  
+    {name:'butterfree',
+      height:15,
+      type:['bug','flying']}
+  ]
+  
+  pokemonList.forEach(function(pokemonList){
+    if(pokemonList.height>16){
+        document.write( '<p>' + pokemonList.name + ' Height: ' + pokemonList.height + ' -Wow, this is big </p>')
+    }
+    else if(pokemonList.height<8){
+        document.write('<p>'+ pokemonList.name + ' Height: ' + pokemonList.height + ' -This is a small pokemon</p>')
+    }else{document.write('<p>' + pokemonList.name + ' Height: '+  pokemonList.height + ' -This is a average size pokemon</p>')}
+  });
+  
+  return {
+  getAll: function(){
+      return pokemonList;
+    },
+  add: function(item){
+      if (typeof pokemon === 'object'){
+      pokemonList.push(item)
+    }else {
+    console.log('this is not a pokemon')
+    };
+    }
+    }
+  })()
+  
+  console.log(pokemonRespository.getAll())
+  
