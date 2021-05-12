7ed739688d4dfff650cceced133ce38d50d73039
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-05-11 22:11:53 -0700

    fixed forLoop so pokemonlist can show

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,27 @@
-let pokemonRespository = (function(){ 
-  let pokemonList=[
-    {name:'balbasure',
-      height:7,
-      type:['grass','poision']},
-  
-    {name:'charizard',
-      height:20,
-      type:['fire','dragon']},
-  
-    {name:'butterfree',
-      height:15,
-      type:['bug','flying']}
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'balbasure', height: 7, type: ['grass', 'poision'] },
+
+    { name: 'charizard', height: 20, type: ['fire', 'dragon'] },
+
+    { name: 'butterfree', height: 15, type: ['bug', 'flying'] },
   ]
-  
-  pokemonList.forEach(function(pokemonList){
-    if(pokemonList.height>16){
-        document.write( '<p>' + pokemonList.name + ' Height: ' + pokemonList.height + ' -Wow, this is big </p>')
-    }
-    else if(pokemonList.height<8){
-        document.write('<p>'+ pokemonList.name + ' Height: ' + pokemonList.height + ' -This is a small pokemon</p>')
-    }else{document.write('<p>' + pokemonList.name + ' Height: '+  pokemonList.height + ' -This is a average size pokemon</p>')}
-  });
-  
-  return {
-  getAll: function(){
-      return pokemonList;
-    },
-  add: function(item){
-      if (typeof pokemon === 'object'){
+  function getAll() {
+    return pokemonList
+  }
+  function add(item) {
+    if (typeof pokemon === 'object') {
       pokemonList.push(item)
-    }else {
-    console.log('this is not a pokemon')
-    };
+    } else {
+      console.log('this is not a pokemon')
     }
-    }
-  })()
-  
-  console.log(pokemonRespository.getAll())
-  
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  }
+})()
+
+pokemonRepository.getAll().forEach(function (item) {
+  console.log(item)})
