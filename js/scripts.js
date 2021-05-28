8030ab20eed6debda8f20b60a263217119208ef0
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-05-27 17:34:06 -0700

    adding Modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-Container');
 
   function getAll() {
     return pokemonList;
   }
+
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -12,21 +14,23 @@ let pokemonRepository = (function () {
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
-          detailsUrl: item.url
+          detailsUrl: item.url,
         };
         add(pokemon);
       });
     }).catch(function (e) {
       console.error(e);
     })
-  }
+  };
+
   function add(pokemon) {
     if (typeof pokemon === 'object' && 'name' in pokemon) {
       pokemonList.push(pokemon)
     } else {
       console.log('this is not a pokemon')
     }
-  }
+  };
+
   function addListItem(pokemon){
     let pokemonlist = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
@@ -39,13 +43,10 @@ let pokemonRepository = (function () {
     })
 
     listItem.appendChild(button);
-    pokemonlist.appendChild(listItem);
-    button.addEventListener("click", function(event) {
-      showDetails(pokemon);
-
-    })
+    pokemonlist.appendChild(listItem); 
   }
 
+  //fetch from api
   function loadDetails(item) {
       let url = item.detailsUrl;
       return fetch(url).then(function (response) {
@@ -54,15 +55,75 @@ let pokemonRepository = (function () {
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
         item.types = [details.types];
+        item.weight = details.weight;
       }).catch(function (e) {
         console.error(e);
       });
-    }
-    function showDetails(item) {
-      pokemonRepository.loadDetails(item).then(function () {
-        console.log(item);
+    };
+
+
+  function showDetails(pokemon) {
+      pokemonRepository.loadDetails(pokemon).then(function () {
+        showModal(pokemon);
       });
+  };
+
+  function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Height : ' + pokemon.height;
+
+    let imageElement = document.createElement('img');
+    imageElement.setAttribute('src', pokemon.imageUrl);
+    imageElement.src = pokemon.imageUrl;
+
+    let weightElement = document.createElement('weight')
+    weightElement.innerText = 'Weight' + pokemon.weight;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imageElement);
+    modal.appendChild(weightElement);
+    modalContainer.appendChild(modal);
+
+
+    modalContainer.classList.add('is-visible');
   }
+
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  };
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content.');
+  });
+
+  window.addEventListener('keydown',(e) => {
+    if (e.key === 'escape' && modalContainer.classList.contains('.is-visible')){
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
   return {
     getAll: getAll,
@@ -72,7 +133,7 @@ let pokemonRepository = (function () {
     loadDetails:loadDetails,
     showDetails: showDetails,
   };
-})()
+})();
 
 pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function(pokemon){
